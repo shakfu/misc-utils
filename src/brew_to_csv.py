c2f5741264c9d8323d1faf9ea2f1a3cb2006bd52
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-06-25 09:20:20 +0300

    added brew-to-csv util

---
+++ src/brew_to_csv.py
@@ -0,0 +1,23 @@
+import csv
+import json
+import os
+
+os.system("brew info --json --installed > brew.json")
+with open('brew.json') as f:
+    pkgs = json.load(f)
+
+with open('names.csv', 'w', newline='') as csvfile:
+    fieldnames = ['name', 'version','desc']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for p in pkgs:
+        # print("name:", p['name'])
+        # print("desc:", p['desc'])
+        # print("version:", p['versions']['stable'])
+        # print()            
+        writer.writerow({
+            'name': p['name'],
+            'version': p['versions']['stable'],
+            'desc': p['desc'],
+        })
